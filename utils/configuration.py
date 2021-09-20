2792c7f607b8415c9578ff5392a97368cd9ba57a
gnswp21 <70848702+gnswp21@users.noreply.github.com>
2021-09-20 22:25:44 +0900

    Update configuration.py

--- utils/configuration.py
+++ utils/configuration.py
@@ -1,4 +1,5 @@
 import json
+from random import shuffle
 from typing import NamedTuple
 
 
@@ -16,6 +17,11 @@ class params(NamedTuple):
     # tsa : linear_schedule
     ################################################################
 
+    # for train
+    case :str = "train_00"
+    ratio : float = 1.0
+    device:'str'='cuda'
+    
     # train    
     seed: int = 1421
     
@@ -26,7 +32,10 @@ class params(NamedTuple):
 
     max_seq_length: int = 128
     train_batch_size: int = 32
-    eval_batch_size: int = 8
+    test_batch_size: int = 8
+    unsup_batch_size: int = 96
+    total_steps: int = 10
+
 
     # unsup
     unsup_ratio: int = 0                # unsup_batch_size = unsup_ratio * sup_batch_size
@@ -35,7 +44,7 @@ class params(NamedTuple):
     need_prepro: bool = False           # is data already preprocessed?
     sup_data_dir: str = None
     unsup_data_dir: str = None
-    eval_data_dir: str = None
+    test_data_dir: str = None
 
 
     @classmethod
